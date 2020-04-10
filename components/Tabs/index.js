20127412f1096b8eee10de7845932b8c321fc852
Ken Donahue <ken.w.donahue@gmail.com>
2020-04-10 12:41:56 -0400

    Tabs are working

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,17 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+    axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => {
+        response.data.topics.forEach(item =>{
+            let topics = document.querySelector('.topics');
+            let tab = document.createElement('div')
+
+            tab.classList.add('tab');
+
+            tab.textContent = item;
+
+            topics.appendChild(tab);
+        })
+    })
+    .catch(error => {console.log(error)})
